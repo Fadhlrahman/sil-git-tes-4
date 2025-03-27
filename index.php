7014b8524c32df77f778f6697d92aca409bb90d3
Fadhlrahman <fitraarsil19@gmail.com>
2025-03-27 13:16:21 +0700

    menambhakan list

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
 </head>
 <body>
     <h1>Hello World</h1>
+   <ul>
+    <li>satu</li>
+    <li>dua</li>
+    <li>tiga</li>
+   </ul>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum cum ab cumque, pariatur magnam aspernatur aliquam veniam est deserunt quos consectetur ex. Sunt architecto libero quae nulla itaque, illo perferendis!</p>
 </body>
 </html>
